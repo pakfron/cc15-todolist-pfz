70628b69a63c34dfe5acd87120b164ac9b177b2e
pakfron <pakpoom.poom@hotmail.com>
2023-08-31 15:15:52 +0700

    Finish:Handle UI

--- src/components/Common/Button/Button.jsx
+++ src/components/Common/Button/Button.jsx
@@ -1,6 +1,13 @@
 import styles from './Button.module.scss';
 
-export function Button({ text, active = true }) {
+// Button(Obj)
+// Button({text,active,type,onClick})
+
+export function Button({ type, onClick, text, active = true }) {
   let btnStyles = active ? styles.btn__primary : styles.btn__secondary;
-  return <button className={`${styles.btn} ${btnStyles}`}>{text}</button>;
+  return (
+    <button className={`${styles.btn} ${btnStyles}`} type={type} onClick={onClick}>
+      {text}
+    </button>
+  );
 }
--- src/components/Todo/TodoForm.jsx
+++ src/components/Todo/TodoForm.jsx
@@ -1,17 +1,73 @@
+import { useState } from 'react';
+import { Button } from '../Common/Button/Button';
 import styles from './TodoForm.module.scss';
+/*
+  props = {
+    textSubmit : string
+  }
+*/
+/*
+CC1- Form Handle
+- ใช้ FN ไปผูกกับ Event ชื่อ onSubmit
+- FN จะถูก Browserเรียกใช้ (เมื่อไหร่ ?) โดยส่ง parameter มา 1 ตัว (event Object)
+- โดย default ทุกปุ่มใน <form> จะทำหน้าที่ submit
+- วิธีแก้ ต้องกำหนด type ของปุ่ม
+  - type="submit" :  <button type='button'>1</button>
+  - type="button" :  <button type='submit'>2</button>
+*/
 
-function TodoForm() {
+/* 
+props = {
+  textSubmit : string
+  setIsOpenForm : FN
+}
+*/
+function TodoForm(props) {
+  const [isError, setIsError] = useState(false);
+  const [taskInput, setTaskInput] = useState('');
+
+  const handleChangeInput = function (event) {
+    if (isError) setIsError(false);
+    setTaskInput(event.target.value);
+  };
+
+  const handleSubmit = function (event) {
+    // 1. PreventDefault
+    event.preventDefault();
+
+    // 2.ต้องรู้ก่อนว่า User พิมพ์อะไร (อยู่ใน state : taskInput)
+
+    // 3.FormValidation
+    // case1 : submit ได้ => ไม่ error
+    // case2 : submit ไม่ได้ => แสดง Error
+    if (taskInput.trim() === '') {
+      console.log('Error');
+      setIsError(true);
+      return;
+    }
+    console.log('submit');
+  };
+
+  const handleCancel = function () {
+    console.log('cancel');
+    // correctName : setIsOpenForm(false)
+    // inCorrectName : undefined(false) => บู้มเป็นโกโก้ครั้นซ์
+    props.setIsOpenForm(false);
+  };
   return (
-    <form className={styles.todo__form__container}>
-      {/*	Body */}
-      <input className={styles.todo__form__input} placeholder='Task Name' />
-
+    <form onSubmit={handleSubmit} className={styles.todo__form__container}>
+      <input
+        className={styles.todo__form__input}
+        placeholder='Task Name'
+        value={taskInput}
+        onChange={handleChangeInput}
+      />
       {/*Form Footer */}
       <div className={styles.todo__form__footer}>
-        <p className={styles.todo__error}>Title is required</p>
+        {isError ? <p className={styles.todo__error}>Title is required</p> : null}
         <div className={styles.todo__form__buttons}>
-          <button>Cancel</button>
-          <button>Add Task</button>
+          <Button text='Cancel' active={false} type='button' onClick={handleCancel} />
+          <Button text={props.textSubmit} active={true} type='submit' />
         </div>
       </div>
     </form>
